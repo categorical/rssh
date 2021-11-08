e8f7de41e16ad9b890944fffbb86647aacbd8090
dawei <dawei.chen@hotmail.co.uk>
2021-11-08 16:58:12 +0000

    a

--- scripts/cygwin/env.sh
+++ scripts/cygwin/env.sh
@@ -29,19 +29,20 @@ _sourcebat "$fenv"
 
 
 
-_field(){
+_configvs(){
+    local k1="$(_esed "$1")"
+    IFS=$'\n' read -r -d $'\0' -a "$2" \
+        < <(sed -n -e "s/^$k1\\s\\+//p" "$fconfig"|sed 's/\s*$//')
+}
+_configfield(){
     local k1="$(_esed "$1")"
     local k2="$(_esed "$2")"
     local k3="$(_esed "$3")"
-    sed -n -e "/^$k1\\s\+$k2/,\${/^\\s*$/q;s/^$k3//p}" "$fconfig"|tr -d '[:space:]'
+    sed -n -e "/^$k1\\s\\+$k2/,\${/^\\s*$/q;s/^$k3//p}" "$fconfig"|tr -d '[:space:]'
 }
 _esed(){ printf '%s' "$1"|sed 's/[.[\*^$/]/\\&/g';}
 _infof(){ local f=$1;shift;printf "\033[96minfo: \033[0m%s\n" "$(printf "$f" "$@")";}
 _errorf(){ local f=$1;shift;printf "\033[91merror: \033[0m%s\n" "$(printf "$f" "$@")";}
-
-
-
-
 
 
 
